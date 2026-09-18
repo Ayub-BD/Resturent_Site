454c21ca67b9466bfae46afc60187f731b6766f5
MD.Abu Ayub Bhuyan <abuayubminhaz290@gmail.com>
2026-09-18 14:19:50 +0600

    Update build.sh

--- build.sh
+++ build.sh
@@ -5,9 +5,6 @@ set -o errexit
 # Install dependencies
 pip install -r requirements.txt
 
-# manage.py ফাইলটি যে ফোল্ডারে আছে সেখানে যাওয়ার জন্য নিচের লাইনটি ঠিক করুন (যদি restaurant_project ফোল্ডারের ভেতরে থাকে)
-cd restaurant_project
-
 # Collect static files & Run migrations
 python manage.py collectstatic --no-input --settings=config.settings
 python manage.py migrate --settings=config.settings
