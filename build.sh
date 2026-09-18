348732bf67eb324235208cbf2cf49d3e5af9c3bf
MD.Abu Ayub Bhuyan <abuayubminhaz290@gmail.com>
2026-09-18 14:16:59 +0600

    build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
+# Exit on error
 set -o errexit
+
 pip install -r requirements.txt
-python -m django collectstatic --no-input
-python -m django migrate
+
+python manage.py collectstatic --no-input --settings=config.settings
+python manage.py migrate --settings=config.settings
